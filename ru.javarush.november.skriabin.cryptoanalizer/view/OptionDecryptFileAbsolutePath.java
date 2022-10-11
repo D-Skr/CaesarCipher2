623adf831bb53b63f4e76fef304f898f3e844389
Di <altusj@gmail.com>
2022-10-11 11:51:20 -0400

    fix decrypt for absolute path

--- ru.javarush.november.skriabin.cryptoanalizer/view/OptionDecryptFileAbsolutePath.java
+++ ru.javarush.november.skriabin.cryptoanalizer/view/OptionDecryptFileAbsolutePath.java
@@ -16,11 +16,11 @@ public class OptionDecryptFileAbsolutePath {
 	static void run(List<Character> alphabet) throws IOException {
 		long currentTime = System.currentTimeMillis();
 		Scanner console = new Scanner(System.in);
-		System.out.println("Please copy your file into 'input' folder");
-		System.out.println("Please enter the name of your text file:");
-		String pathNotEncryptedFile = "input/" + console.nextLine();
+		System.out.println("Please enter the absolute path and name of your text file:");
+		String pathNotEncryptedFile = console.nextLine();
 		System.out.println(pathNotEncryptedFile);
-		String pathEncryptedFile = "output/decrypted" + currentTime + ".txt";
+		String pathEncryptedFile = pathNotEncryptedFile.substring(0, pathNotEncryptedFile.length() - 4) + "_decrypted_"
+				+ currentTime + ".txt";
 		System.out.println("Please enter shift key:");
 		int shift = Integer.parseInt(console.nextLine());
 		System.out.println("Your result saved in: " + pathEncryptedFile);
